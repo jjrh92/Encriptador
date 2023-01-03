9c6bbd319af2f666d339e1160b22190de99bc156
Julio Reyes <juliojesusreyes@gmail.com>
2023-01-03 13:01:52 -0300

    Terminando JS

--- main.js
+++ main.js
@@ -5,16 +5,14 @@ const Rectangle1_texto2 = document.getElementById ("Rectangle1_texto2");
 const Frame5 = document.getElementById ("Frame5");
 const botonEncriptar = document.getElementById ("botonEncriptar");
 const botonDesencriptar = document.getElementById ("botonDesencriptar");
-const vocalesEncriptar = {"a":"ai", "e":"enter", "i":"imes", "o":"ober", "u":"ufat","á":"ai", "é":"enter", "í":"imes", "ó":"ober", "ú":"ufat","à":"ai", "è":"enter", "ì":"imes", "ò":"ober", "ù":"ufat"};
-const vocalesDesencriptar = {"ai":"a", "enter":"e", "imes":"e", "ober":"o", "ufat":"u"};
 const textarea = document.createElement ("textarea"); // Al crear este elemento fuera del scope de la funcion prevenimos la superposicion del texto.
 const botonCopiar = document.createElement ("button"); // Este boton llevara el texto encriptado/desencriptado al portapapeles.
 
 function RemoverDefault () {
 
-    munieco.remove();
-    Rectangle1_texto1.remove();
-    Rectangle1_texto2.remove();
+    munieco.remove ();
+    Rectangle1_texto1.remove ();
+    Rectangle1_texto2.remove ();
 
 }
 
@@ -26,25 +24,27 @@ function Encriptar () {
     if (textoMinusculas == "" || textoMinusculas == +textoMinusculas) {
 
         alert ("No hay texto para encriptar o se ingresó un valor numerico.");
-        window.location.reload();
+        window.location.reload ();
 
     } else {
 
         RemoverDefault ();
-        let textoEncriptado = textoMinusculas.replace(/[aeiouáéíóúàèìòù]/g, clave => vocalesEncriptar[clave]);
+        let textoEncriptado = textoMinusculas.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
         textarea.value = textoEncriptado;
         textarea.spellcheck = false;
         textarea.className = "textoSalida";
-        Frame5.append(textarea);
+        Frame5.append (textarea);
         botonCopiar.className = "btn_co";
         botonCopiar.innerText = "Copiar";
-        Frame5.append(botonCopiar);
+        Frame5.append (botonCopiar);
         botonEncriptar.innerText = "Encriptación Exitosa!";
 
         botonCopiar.onclick = function () {
+            
             let textoCopiado = textoEncriptado;
             navigator.clipboard.writeText (textoCopiado);
             botonCopiar.innerText = "Texto copiado a portapapeles!";
+
         }
 
     }
@@ -64,14 +64,14 @@ function Desencriptar () {
     } else {
 
         RemoverDefault ();
-        let textoDesencriptado = textoMinusculas.replace (vocalesDesencriptar, /[aienterimesoberufat]/g);
+        let textoDesencriptado = textoMinusculas.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
         textarea.value = textoDesencriptado;
         textarea.spellcheck = false;
         textarea.className = "textoSalida";
-        Frame5.append(textarea);
+        Frame5.append (textarea);
         botonCopiar.className = "btn_co";
         botonCopiar.innerText = "Copiar";
-        Frame5.append(botonCopiar);
+        Frame5.append (botonCopiar);
         botonDesencriptar.innerText = "Desencriptación Exitosa!";
 
 
